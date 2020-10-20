20ac6871209409b137ca96ba9045d8d693d7a7a5
goodxodyd11 <wjdxodyd11@gmail.com>
2020-10-20 19:49:13 +0900

    [Revise] combine all code and add button thread

--- controller.py
+++ controller.py
@@ -5,6 +5,7 @@ from view import view
 from preprocessing import preprocessing
 from translator import translator
 from voice import voice
+import buttons
 
 
 def main():
@@ -12,28 +13,36 @@ def main():
     prep = preprocessing()
     trans = translator()
     announcer = voice()
+    buttons.gpio_init()
+    button_thread = buttons.Thread(target=buttons.thread_voice_flag, args=(1, event_handler))
+    button_thread.start()
+
     while True:
         voc_event = event_handler.voice_flag
         cap_event = event_handler.capture_flag
+        ret_val, cap_img = event_handler.handle_capture()
+        event_handler.show_img(cap_img)
 
         if voc_event:
-            cap_img = event_handler.handle_voice()
-            paragraph_list, is_eng = prep.preprocess_img(cap_img)
+            print("hello")
+            preprocessed_img = prep.preprocess_img(cap_img)
+            paragraph_list = [prep.run_tesseract(preprocessed_img)]
+            print(paragraph_list)
+        #
+        #     if is_eng:
+        #         paragraph_list = trans.translate(paragraph_list, 'ko')
+        #
+        #     ptr_voice = announcer.store_voice(paragraph_list)  # default en
+        #
+        #     success = event_handler.print_voice(ptr_voice)
+        #
+        #     if not success:
+        #         event_handler.print_error()
+        #
+            event_handler.set_voice_flag = False
 
-            if is_eng:
-                paragraph_list = trans.translate(paragraph_list, 'ko')
-
-            ptr_voice = announcer.store_voice(paragraph_list)  # default en
-
-            success = event_handler.print_voice(ptr_voice)
-
-            if not success:
-                event_handler.print_error()
-
-        if cap_event:
-            event_handler.handle_capture()
-
-        time.sleep(1)
+        # time.sleep(1)
+        # print("next")
 
 
 if __name__ == '__main__':
--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,15 @@
+import cv2
+import pytesseract
 
 class preprocessing(object):
     def __init__(self):
         pass
 
+
     def preprocess_img(self, img):
-        pass
+        return img
+
+
+    def run_tesseract(self, img):
+        detected_text = pytesseract.image_to_string(img)
+        return detected_text
